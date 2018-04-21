f8eb91b9bd3ea53389916ab8f4079cde1627e14f
Cindy <33848023+cindyjialiu@users.noreply.github.com>
2018-04-21 22:21:38 +0100

    Create test for sorting items

---
+++ test for sorting.rb
@@ -0,0 +1,13 @@
+test_array1=["cow","horse"]
+swap_items(test_array1, 0)
+puts "test_array1  = #{test_array1}"
+
+test_array2=["pig","dog","cat"]
+swap_items(test_array2, 1)
+puts "test_array_pcd  = #{test_array2}"
+
+test_array=["banana","dog","orange","pear","date"]
+swap_items(test_array, 3)
+swap_items(test_array, 2)
+swap_items(test_array, 0)
+puts "test_array_pcd  = #{test_array}"
