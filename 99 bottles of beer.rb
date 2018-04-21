f760854b96147841f2aeefc585793863838fbe7b
Cindy <33848023+cindyjialiu@users.noreply.github.com>
2018-04-21 22:08:38 +0100

    Create 99bottles of beer

---
+++ 99 bottles of beer.rb
@@ -0,0 +1,8 @@
+n=99
+while n!=0
+  puts "#{n} bottles of beer on the wall, #{n} bottles of beer.
+Take one down and pass it around, #{n-1} bottles of beer on the wall"
+n-=1
+end
+puts "No more bottles of beer on the wall, no more bottles of beer. 
+Go to the store and buy some more, 99 bottles of beer on the wall."
